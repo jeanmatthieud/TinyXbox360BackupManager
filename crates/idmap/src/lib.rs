0bf8a1c87f886d47fdf7d7a2bd5f7b403b40e5eb
Manuel Quarneti <mq1@ik.me>
2026-05-30 00:50:11 +0200

    optimize

--- crates/idmap/src/lib.rs
+++ crates/idmap/src/lib.rs
@@ -13,36 +13,52 @@ struct AlignedBytes<const N: usize>([u8; N]);
 static ALIGNED_BYTES: AlignedBytes<BIN_LEN> =
     AlignedBytes(*include_bytes!(concat!(env!("OUT_DIR"), "/id_map.bin")));
 
+#[inline(always)]
+fn game_ids() -> &'static [u32] {
+    unsafe { std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().cast::<u32>(), COUNT) }
+}
+
+#[inline(always)]
+fn ghids() -> &'static [u32] {
+    unsafe { std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().cast::<u32>().add(COUNT), COUNT) }
+}
+
+#[inline(always)]
+fn title_offsets() -> &'static [u32] {
+    unsafe {
+        std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().cast::<u32>().add(COUNT * 2), COUNT)
+    }
+}
+
+#[inline(always)]
+fn title_lengths() -> &'static [u8] {
+    unsafe { std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().add(COUNT * 12), COUNT) }
+}
+
 fn find(id: u32) -> Option<usize> {
-    let ptr = ALIGNED_BYTES.0.as_ptr().cast::<u32>();
-    let game_ids = unsafe { std::slice::from_raw_parts(ptr, COUNT) };
-    game_ids.binary_search(&id).ok()
+    game_ids().binary_search(&id).ok()
 }
 
 pub fn get_ghid(id: u32) -> Option<NonZeroU32> {
     let idx = find(id)?;
-
-    let offset = COUNT * 4 + idx * 4;
-    let ghid = u32::from_ne_bytes(ALIGNED_BYTES.0[offset..offset + 4].try_into().unwrap());
-
+    let ghid = unsafe { *ghids().get_unchecked(idx) };
     NonZeroU32::new(ghid)
 }
 
 pub fn get_title(id: u32) -> Option<&'static str> {
     let idx = find(id)?;
 
-    let relative_title_offset = {
-        let offset = COUNT * 8 + idx * 4;
-        u32::from_ne_bytes(ALIGNED_BYTES.0[offset..offset + 4].try_into().unwrap())
+    let relative_title_offset = unsafe { *title_offsets().get_unchecked(idx) } as usize;
+    let title_len = unsafe { *title_lengths().get_unchecked(idx) } as usize;
+
+    let title_offset = COUNT * 13 + relative_title_offset;
+
+    let title_slice = unsafe {
+        ALIGNED_BYTES
+            .0
+            .get_unchecked(title_offset..title_offset + title_len)
     };
 
-    let title_len = {
-        let offset = COUNT * 12 + idx;
-        ALIGNED_BYTES.0[offset] as usize
-    };
-
-    let title_offset = relative_title_offset as usize + COUNT * 13;
-    let title_slice = &ALIGNED_BYTES.0[title_offset..title_offset + title_len];
     let title = unsafe { std::str::from_utf8_unchecked(title_slice) };
 
     Some(title)
