a35e38ac99e7f3b944dbbe0abee11caf28e4efa0
Manuel Quarneti <mq1@ik.me>
2026-05-30 12:31:35 +0200

    refactoring

--- crates/idmap/src/lib.rs
+++ crates/idmap/src/lib.rs
@@ -35,6 +35,12 @@ fn title_lengths() -> &'static [u8] {
     unsafe { std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().add(COUNT * 12), COUNT) }
 }
 
+#[inline(always)]
+fn titles() -> &'static [u8] {
+    const TITLES_LEN: usize = BIN_LEN - COUNT * 13;
+    unsafe { std::slice::from_raw_parts(ALIGNED_BYTES.0.as_ptr().add(COUNT * 13), TITLES_LEN) }
+}
+
 fn find(id: u32) -> Option<usize> {
     game_ids().binary_search(&id).ok()
 }
@@ -48,17 +54,10 @@ pub fn get_ghid(id: u32) -> Option<NonZeroU32> {
 pub fn get_title(id: u32) -> Option<&'static str> {
     let idx = find(id)?;
 
-    let relative_title_offset = unsafe { *title_offsets().get_unchecked(idx) } as usize;
+    let title_offset = unsafe { *title_offsets().get_unchecked(idx) } as usize;
     let title_len = unsafe { *title_lengths().get_unchecked(idx) } as usize;
 
-    let title_offset = COUNT * 13 + relative_title_offset;
-
-    let title_slice = unsafe {
-        ALIGNED_BYTES
-            .0
-            .get_unchecked(title_offset..title_offset + title_len)
-    };
-
+    let title_slice = unsafe { titles().get_unchecked(title_offset..title_offset + title_len) };
     let title = unsafe { std::str::from_utf8_unchecked(title_slice) };
 
     Some(title)
